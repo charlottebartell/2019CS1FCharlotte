e0dccb85c1ff1a351302700a24ef576332d469f3
MrArturtleNSCDS <agarcia@nscds.org>
2019-12-18 15:17:24 +0000

    Left some comments and corrections

--- national-parks/click.js
+++ national-parks/click.js
@@ -1,6 +1,9 @@
 $('.circle').click(
       function(){
-       $("#box").fadeIn();
+        var name = $('.name',this).text(); //read the text from the .name div in the clicked circle
+        $('.name','#box').text(name); // write the above result in the .name div in #box
+
+        $("#box").fadeIn();
     }
 
 );
@@ -10,5 +13,4 @@ $('#box').click(
       function(){
        $(this).fadeOut();
     }
-
 );
