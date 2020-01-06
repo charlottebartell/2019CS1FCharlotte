23c7b92ea6dcce52dfb6c3b0c0624b707a57d741
charlottebartell <cbartell22@nscds.org>
2020-01-06 17:44:19 +0000

    new

--- national-parks/click.js
+++ national-parks/click.js
@@ -3,6 +3,10 @@ $('.circle').click(
         var name = $('.name',this).text(); //read the text from the .name div in the clicked circle
         $('.name','#box').text(name); // write the above result in the .name div in #box
         console.log (name,$(this).attr('id'));
+
+        
+        
+        
         $("#box").fadeIn();
     }
 
