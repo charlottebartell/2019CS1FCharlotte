a659304d86234bb10cc020c63c9409475be19c98
charlottebartell <cbartell22@nscds.org>
2020-01-09 21:04:50 +0000

    js description

--- national-parks/click.js
+++ national-parks/click.js
@@ -3,8 +3,10 @@ $('.circle').click(
         var name = $('.name',this).text(); //read the text from the .name div in the clicked circle
         $('.name','#box').text(name); // write the above result in the .name div in #box
         console.log (name,$(this).attr('id'));
-
         
+        var description= $('.description',this).text(); 
+        $('.description','#box').text(description); 
+        console.log (description,$(this).attr('id'));
         
         
         $("#box").fadeIn();
